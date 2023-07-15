90f453b4f09e85c0a651c8c8f48b1afc26106837
Kelello-7206 <tseripe7@gmail.com>
2023-07-15 15:29:05 +0200

    added event listener

--- PH Mall/javascript/contact.js
+++ PH Mall/javascript/contact.js
@@ -1,16 +1,16 @@
-function showHiddenRows() {
-        
-    const hiddenRows = document.getElementsByClassName('hide');
-    let tableBtn = document.querySelector('[data-bnt]');
+const showHiddenRows = () => {
+  const hiddenRows = document.getElementsByClassName('hide');
+  let tableBtn = document.querySelector('[data-bnt]');
 
-    for (let i = 0; i < hiddenRows.length; i++) {
-      if (hiddenRows[i].style.display === 'none') {
-        hiddenRows[i].style.display = 'table-row';
-        tableBtn.innerHTML= "Show Less";
-      } else {
-        hiddenRows[i].style.display = 'none';
-        tableBtn.innerHTML= "Show More";
-      }
+  for (let i = 0; i < hiddenRows.length; i++) {
+    if (hiddenRows[i].style.display === 'none') {
+      hiddenRows[i].style.display = 'table-row';
+      tableBtn.innerHTML = "Show Less";
+    } else {
+      hiddenRows[i].style.display = 'none';
+      tableBtn.innerHTML = "Show More";
     }
+  }
+}
 
-  };
+document.getElementById('showMoreButton').addEventListener('click', showHiddenRows);
